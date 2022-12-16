284c5c7223b5483990313b9f092a7f060500b40b
MikeSindani <mikems@live.fr>
2022-12-16 15:34:51 +0200

    asyfa safe

---
+++ index.js
@@ -0,0 +1,12 @@
+let age 
+let name
+let prename
+name = "mike"
+prename = "sindani"
+age = 21
+age = age + 1
+console.log("Age  " + age)
+console.log("hello ",name)
+
+document.getElementById("p1").innerText = "hello "+ name
+document.getElementById("p2").innerText = prename
